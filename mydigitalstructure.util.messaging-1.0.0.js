5337a0b95a9028bdbb7579ce081e58b2df566342
ibCom <hello@ibcom.biz>
2021-10-11 18:47:41 +1100

    Create mydigitalstructure.util.messaging-1.0.0.js

---
+++ mydigitalstructure.util.messaging-1.0.0.js
@@ -0,0 +1,30 @@
+/*
+	{
+    	title: "Util; Messaging",
+  	}
+
+  	https://docs.mydigitalstructure.cloud/endpoint_messaging
+*/
+
+mydigitalstructure._util.factory.messaging = function (param)
+{
+	app.add(
+	[
+        {
+            name: 'util-messaging-email-inbox-init',
+            notes: 'Initialise an inbox',
+            code: function (param)
+            {
+
+            }
+	    },
+        {
+            name: 'util-messaging-email-inbox-refresh',
+            notes: 'Refresh an inbox',
+            code: function (param)
+            {
+
+            }
+        }
+    ]);
+}
